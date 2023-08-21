3a94896bb9e2b444d0d14cb3bc5ef674ea72de07
Viktor Ilie <viktor.ilie@outlook.com>
2023-08-21 13:07:43 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import "./App.css";
-import ShoppingList from "./ShoppingList";
-
-const data = [
-  { id: 1, item: "eggs", quantity: 12, completed: false },
-  { id: 2, item: "milk", quantity: 1, completed: true },
-  { id: 3, item: "chicken breast", quantity: 4, completed: false },
-  { id: 4, item: "carrots", quantity: 6, completed: true },
-];
 
 function App() {
   return (
     <div>
-      <ShoppingList items={data} />
+      <h1>hello treact</h1>
     </div>
   );
 }
